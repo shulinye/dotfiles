ac9c2195a3d8b2d9a80e9759bc7d1f202715c1a0
Shulin Ye <nightshadequeen@gmail.com>
2014-08-27 16:34:49 -0500

    add $HOST

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "backing up current packages"
-dpkg --get-selections > $HOME/installed_packages_`date +%F`.txt
-sudo cp /etc/apt/sources.list $HOME/sources-`date +%F`.list
-sudo cp /etc/apt/sources.list.d ~/sources-`date +%F`.list.d -r
-sudo apt-key exportall > ~/repo-`date +%F`.keys
+dpkg --get-selections > $HOME/$HOST-installed_packages_`date +%F`.txt
+sudo cp /etc/apt/sources.list $HOME/$HOST-sources-`date +%F`.list
+sudo cp /etc/apt/sources.list.d ~/$HOST-sources-`date +%F`.list.d -r
+sudo apt-key exportall > ~/$HOST-repo-`date +%F`.keys
 
 
 echo "making symlinks"
