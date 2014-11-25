b4b1c986db246f39c8245048ad7efe615b29c3db
Shulin Ye <nightshadequeen@gmail.com>
2014-11-25 10:52:04 -0600

    adding a custom fasd plugin with custom functions

---
+++ zsh/fasd.plugin.zsh
@@ -0,0 +1,24 @@
+#default fasd plugin
+
+if [ $commands[fasd] ]; then # check if fasd is installed
+  fasd_cache="$HOME/.fasd-init-cache"
+  if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+    fasd --init auto >| "$fasd_cache"
+  fi
+  source "$fasd_cache"
+  unset fasd_cache
+  alias v='f -e vim'
+  alias o='a -e open'
+
+fi
+
+#custom fasd functions
+function goto {
+        FILE=$(f $1 | head -n1)
+        if [ -z "$FILE" ]; then
+            echo "Nothing found"
+        else
+            echo $FILE 
+            cd `dirname $FILE`
+        fi
+   }
