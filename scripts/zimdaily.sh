21d0660e0742549d0e854bc6eb2592513edb7555
Shulin Ye <nightshadequeen@gmail.com>
2015-01-05 09:56:01 -0600

    minor bugfixes, because I apparentaly fail at file redirection

--- scripts/zimdaily.sh
+++ scripts/zimdaily.sh
@@ -29,16 +29,6 @@ Creation-Date: $(date +"%FT%T%:z") \n
 @journal @diary \n
 ==== Tasks ==== \n\n" > "$TODAY"
 
-echo -e "=== FROM YESTERDAY ===\n\n" >> "$TODAY"
-movetasks "$YESTERDAY"
-if [ -f "$YESTERDAY" ] ; then
-    sed -i -e '/~~/!s/\[ \] /\[x\] ~~/' -e '/~~.*~~/!s/~~.*$/&~~/' "$YESTERDAY"
-    #Strike out yesterday's tasks.
-    cd "$JOURNALDIR"
-    git add "$YESTERDAY"
-    git commit "$YESTERDAY" -m "Moving tasks from $(date -d 'yesterday' +%F) over to $(date +%F)"
-fi
-
 echo -e "=== FROM TODO ===\n\n" >> "$TODAY"
 movetasks "$TODO"
 
@@ -46,12 +36,26 @@ echo -e "=== FROM DAILY ===\n\n" >> "$TODAY"
 movetasks "$DAILY"
 
 if [ $(date +%u) = $DoW ] ; then
-    echo -e "=== FROM WEEKLY ===\n\n"
+    echo -e "=== FROM WEEKLY ===\n\n" >> "$TODAY"
     movetasks "$WEEKLY"
+fi
+
+echo -e "=== FROM YESTERDAY ===\n\n" >> "$TODAY"
+movetasks "$YESTERDAY"
+if [ -f "$YESTERDAY" ] ; then
+    if [ "$(grep -c '\[ \]' "$YESTERDAY" )" -gt 0 ] ; then
+        sed -i -e '/~~/!s/\[ \] /\[x\] ~~/' -e '/~~.*~~/!s/~~.*$/&~~/' "$YESTERDAY"
+        #Strike out yesterday's tasks.
+        cd "$JOURNALDIR"
+        git add "$YESTERDAY"
+        git commit "$YESTERDAY" -m "Moving tasks from $(date -d 'yesterday' +%F) over to $(date +%F)"
+    fi
 fi
 
 echo -e "==== Tasks Completed ==== \n
 ==== Diary ==== \n\n" >> "$TODAY"
+
+cat -s "$TODAY" | sponge "$TODAY"
 
 cd "$DIR"
 git add "$TODAY"
