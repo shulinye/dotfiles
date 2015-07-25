69669de13d9528cb1acd4dc52b040405d24d3930
Shulin Ye <nightshadequeen@gmail.com>
2015-07-25 14:03:14 -0500

    moves things into their own folder

---
+++ scripts/zim/zimdaily.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+source zimcommon.sh
+
+TAGS="@journal @diary @Year$(date +%Y)"
+
+if [ ! -f "$TODAY" ] ; then
+
+echo -e "Content-Type: text/x-zim-wiki
+Wiki-Format: $(zim --version | head -n1)
+Creation-Date: $(date +"%FT%T%:z") \n
+====== $(date +"%A %d %b %Y") ====== \n
+Week $(date +%V)\n
+$TAGS \n
+==== Quote ==== \n
+$(fortune)\n
+==== Tasks ==== \n\n" > "$TODAY"
+
+echo -e "=== FROM TODO ===\n\n" >> "$TODAY"
+movetasks "$TODO" "$TODAY"
+
+echo -e "=== RTM ===\n\n" >> "$TODAY"
+if wget -q --spider rememberthemilk.com; then
+    echo -e "$(head -n8 $RTMMIRROR) \n $(./zimrss.py "${@:2}")" | sponge "$RTMMIRROR"
+else
+    echo -e "Internet down, from mirror"
+fi
+if [ -f "$RTMMIRROR" ] ; then
+    grep "$(date +'%d %b %y')" "$RTMMIRROR" >> "$TODAY"
+fi
+
+echo -e "=== FROM DAILY ===\n\n" >> "$TODAY"
+movetasks "$DAILY" "$TODAY"
+
+if [ "$(date +%u)" = "$DoW" ] ; then
+    echo -e "=== FROM WEEKLY ===\n\n" >> "$TODAY"
+    movetasks "$WEEKLY" "$TODAY"
+fi
+
+echo -e "=== FROM YESTERDAY ===\n\n" >> "$TODAY"
+movetasks "$YESTERDAY" "$TODAY"
+if [ -f "$YESTERDAY" ] ; then
+    if [ "$(grep -c '\[ \]' "$YESTERDAY" )" -gt 0 ] ; then
+        sed -i -e '/~~/!s/\[ \] /\[x\] ~~/' -e '/~~.*~~/!s/~~.*$/&~~/' "$YESTERDAY"
+        #Strike out yesterday's tasks.
+        "$HOME/.dotfiles/scripts/dedupzim.py" < "$YESTERDAY" | sponge "$YESTERDAY" #little bit of cleanup
+        cd "$JOURNALDIR" &&
+        git add "$YESTERDAY" &&
+        git commit "$YESTERDAY" -m "Moving tasks from $(date -d 'yesterday' +%F) over to $(date +%F)"
+    fi
+fi
+
+echo -e "==== Tasks Completed ==== \n
+==== Diary ==== \n\n" >> "$TODAY"
+
+
+cat -s "$TODAY" |\
+        #Remove extraneous whitespace
+    "$HOME/.dotfiles/scripts/dedupzim.py" |\
+        #Remove duplicate lines
+    sponge "$TODAY"
+
+cd "$DIR" || exit
+git add "$TODAY"
+git commit "$TODAY" -m "Initial commit: $(date +%F)"
+fi
+
+ln -sf "$TODAY" "/home/shulinye/Dropbox/todo/today"
---
+++ scripts/zim/zimmonthly.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source zimcommon.sh
+
+THISMONTH="$DIR.txt"
+LASTMONTH="$JOURNALDIR/`date +'%Y/%m.txt' -d 'last month'`"
+MONTHLY="$NOTESDIR/1_-_to-Do/Monthly_Tasks.txt"
+TAGS="@Year$(date +%Y)"
+if [ ! -f "$THISMONTH" ] ; then
+
+echo -e "Content-Type: text/x-zim-wiki
+Wiki-Format: $(zim --version | head -n1)
+Creation-Date: $(date +"%FT%T%:z") \n
+====== $(date +"%B %Y") ====== \n
+$TAGS \n
+==== Quote ==== \n
+$(fortune)\n
+==== Tasks ==== \n\n" > "$THISMONTH"
+
+
+echo -e "=== FROM LAST MONTH ($(date +'%B %Y' -d 'last month')) ===\n\n" >> "$THISMONTH"
+movetasks "$LASTMONTH" "$THISMONTH"
+
+if [ -f "$LASTMONTH" ] ; then
+    if [ "$(grep -c '\[ \]' "$LASTMONTH" )" -gt 0 ] ; then
+        sed -i -e '/~~/!s/\[ \] /\[x\] ~~/' -e '/~~.*~~/!s/~~.*$/&~~/' "$LASTMONTH"
+        #Strike out yesterday's tasks.
+        "$HOME/.dotfiles/scripts/dedupzim.py" < "$LASTMONTH" | sponge "$LASTMONTH" #little bit of cleanup
+        cd "$JOURNALDIR"
+        git add "$LASTMONTH"
+        git commit "$LASTMONTH" -m "Moving tasks from $(date -d 'last month' +'%B %Y') over to $(date +'%B %Y')"
+    fi
+fi
+
+
+echo -e "=== MONTHLY TASKS ===\n\n" >> "$THISMONTH"
+movetasks "$MONTHLY" "$THISMONTH"
+
+cd $DIR
+git add "$THISMONTH"
+git commit "$THISMONTH" -m "Initial commit: $(date +'%B %Y')"
+
+fi
+
