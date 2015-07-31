08cfa97c2fe641fcb3697468aae33275d4de905c
Shulin Ye <nightshadequeen@gmail.com>
2015-07-30 22:13:28 -0500

    adding element weights

--- scripts/chemistryutils/molarity.py
+++ scripts/chemistryutils/molarity.py
@@ -5,6 +5,9 @@
 from enum import Enum
 import re
 
+ELEMENT_FILE = "elements.txt"
+MOLECULAR_WEIGHT_FILE = "molecular_weights.txt"
+
 class Conversions(Enum):
     re_format = re.compile(r'(\d+)\s*([a-z]+)')
     k = 10**3
@@ -12,8 +15,32 @@ class Conversions(Enum):
     u = 10**(-6)
     micro = 10**(-6)
 
+class Element(object):
+    def __init__(self, mass, name, symbol, number):
+        self.mass = mass
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+    def __hash__(self):
+        return hash(self.name)
+    def __repr__(self):
+        return "%s(%s, %d)" % (self.__class__.__name__, self.name, self.number)
+    def __eq__(self, other):
+        if not isinstance(other, Element): return False
+        return self.number == other.number
+    def __lt__(self, other):
+        return self.number < other.number
+
+elements = {}
+with open(ELEMENT_FILE) as f:
+    for i in f:
+        mass, name, symbol, number = i.split()
+        mass = float(mass)
+        number = int(number)
+        elements[symbol] = Element(mass, name, symbol, number)
+
 chemicals = {}
-with open("molecular_weights.txt") as f:
+with open(MOLECULAR_WEIGHT_FILE) as f:
     for i in f:
         k,v = i.split(" = ")
         chemicals[k] = float(v)
