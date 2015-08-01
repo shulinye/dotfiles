3caae6da5ffff24e4b859de0872b84bd949e3858
Shulin Ye <shulinye@mit.edu>
2015-08-01 18:12:27 -0500

    everything must be string :P

--- scripts/pythonutils/autorepr.py
+++ scripts/pythonutils/autorepr.py
@@ -5,4 +5,4 @@ import inspect
 def autorepr(instance):
     sig = inspect.signature(instance.__init__)
     param = iter(sig.parameters)
-    return "%s(%s)" % (instance.__class__.__name__, ", ".join("%s=%s" % (i, getattr(instance,i)) for i in param))
+    return "%s(%s)" % (instance.__class__.__name__, ", ".join("%s=%s" % (i, repr(getattr(instance,i))) for i in param))
