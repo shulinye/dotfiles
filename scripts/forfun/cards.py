0c7aa714f0b58b218b5f9db9e2489762269911a2
Shulin Ye <nightshadequeen@gmail.com>
2015-08-07 22:07:45 -0500

    small change in logic

--- scripts/forfun/cards.py
+++ scripts/forfun/cards.py
@@ -6,7 +6,11 @@ from itertools import product
 
 __all__ = ['Card', 'Deck']
 
+from functools import total_ordering
+
+@total_ordering
 class Card(object):
+    """Virtual playing card"""
     suit_map = ['Clubs','Diamonds', 'Hearts', 'Spades']
     rank_map = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
     def __init__(self, suit, rank):
@@ -15,8 +19,11 @@ class Card(object):
     def __repr__(self):
         return '<%s: %s of %s>' % (self.__class__.__name__, self.rank_map[self.rank], self.suit_map[self.suit])
     def __eq__(self, other):
+        if not isinstance(other, Card): return False
         return self.suit == other.suit and self.rank == other.rank
     def __lt__(self, other):
+        if not isinstance(other, Card):
+            raise TypeError("unorderable types, %s < %s" % (type(self).__name__, type(other).__name__))
         if self.rank == other.rank:
             return self.suit < other.suit
         if self.rank == 0: # I have an Ace!
@@ -34,7 +41,15 @@ class Deck(deque):
         else:
             self.extend(cards)
         shuffle(self)
-    def deal(self, n=1):
-        return [self.popleft() for _ in range(n)]
+    def deal(self, n=2):
+        hands = [Deck([]) for _ in range(n)]
+        while self:
+            for i in hands:
+                i.appendleft(self.popleft())
+        return hands
     def shuffle(self):
         shuffle(self)
+    def popleft(self, n = 1):
+        return [super(Card, self).popleft() for _ in range(n)]
+    def pop(self, n = 1):
+        return [super(Card, self).pop() for _ in range(n)]
