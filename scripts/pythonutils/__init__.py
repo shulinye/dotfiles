c906db1da13fe3a478180d633401c0869b06379c
Shulin Ye <nightshadequeen@gmail.com>
2015-08-10 14:38:37 -0500

    now with more lookup options

--- scripts/pythonutils/__init__.py
+++ scripts/pythonutils/__init__.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 """Various useful utilties for python"""
 
 from .autorepr import *
-from .colorful import *
 from .coerced_input import *
+from .colorful import *
 from .context import *
-from .heapq_wrapper import *
 from .filing import *
+from .heapq_wrapper import *
 from .testing import *
