a99573a0eb8a20a1f2178fbfaf878317713d6856
Shulin Ye <nightshadequeen@gmail.com>
2015-08-13 16:44:40 -0500

    more silly decorator/context-manager stuff.
    I am having wayyyyy too much fun with this

--- scripts/pythonutils/colorful.py
+++ scripts/pythonutils/colorful.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
-__all__ = ['color']
+__all__ = ['color', 'ColorBlock']
+
+import colorama
+from functools import wraps
+from .autorepr import autorepr
+
+colorama.init()
 
 class color(object):
    PURPLE = '\033[95m'
@@ -14,3 +20,20 @@ class color(object):
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
+@autorepr
+class ColorBlock(object):
+    __slots__= ['colorcode']
+    def __init__(self, colorcode):
+        self.colorcode = colorcode
+    def __enter__(self):
+        print(getattr(color, self.colorcode, self.colorcode), end='')
+    def __exit__(self, etype, val, trace):
+        print(colorama.Style.RESET_ALL, end='')
+    def __call__(self, func):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            print(getattr(color, self.colorcode, self.colorcode), end='')
+            ret = func(*args, **kwargs)
+            print(colorama.Style.RESET_ALL, end='')
+            return ret
+        return decorated
