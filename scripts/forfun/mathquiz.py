04a988bf87c2e11c097800654af8a14ae076cc31
Shulin Ye <nightshadequeen@gmail.com>
2015-08-26 13:24:08 -0500

    more stuff, now with zeros and a more robust division system

--- scripts/forfun/mathquiz.py
+++ scripts/forfun/mathquiz.py
@@ -3,7 +3,7 @@
 import operator
 import random
 import signal
-from decimal import Decimal, InvalidOperation
+from time import sleep
 
 mapping = {'+': operator.add,
            '*': operator.mul,
@@ -24,31 +24,48 @@ def input_with_timeout(prompt, timeout=TIMEOUT):
         signal.alarm(0)
         signal.signal(signal.SIGALRM, old_handler)
 
-def ask_question():
-    a,b,c = [random.randint(1,10) for _ in range(3)]
+def ask_question(timeout=TIMEOUT):
+    a,b,c = [random.randint(10) for _ in range(3)]
     op1, op2 = random.sample(list(mapping),2)
-    ans = mapping[op2](mapping[op1](*map(Decimal,(a,b))), Decimal(c))
+    ans = mapping[op2](mapping[op1](a,b), c)
     try:
         prompt = "(%d %s %d) %s %d = " % (a, op1, b, op2, c)
-        if ans == Decimal(input_with_timeout(prompt)):
+        userans = input_with_timeout(prompt, timeout)
+        if "exit" in userans: raise KeyboardInterrupt
+        if abs(ans - float(userans)) < 0.001:
             print("Correct!")
             return True
     except TimeoutError:
         print("\nTimed out: answer was", ans)
-    except InvalidOperation:
+        raise
+    except ValueError:
         print("Invalid input: answer was", ans)
+        raise
     else:
         print("Incorrect: answer is", ans)
 
-def main():
+def main(timeout=TIMEOUT):
     score = 0
+    incorrect = 0
+    timedout_count = 0
     try:
         while True:
-            if ask_question():
-                score += 1
+            try:
+                if ask_question(timeout=TIMEOUT):
+                    score += 1
+                else:
+                    incorrect += 1
+            except TimeoutError:
+                timedout_count += 1
+            except ValueError:
+                pass
+            except ZeroDivisionError:
+                pass
+            sleep(0.1)
     except KeyboardInterrupt:
-        print('\nCorrect answers:',score)
+        print('\nCorrect answers: {}\nIncorrect: {}\nTimed out: {}'.format(score, incorrect, timedout_count))
 
 if __name__ == "__main__":
-    main()
-
+    import sys
+    if len(sys.argv) >1 : main(float(sys.argv[1]))
+    else: main()
