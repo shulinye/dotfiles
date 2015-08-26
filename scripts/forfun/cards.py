2bf87ac762148b025a27e1eb191a2a5672188cf9
Shulin Ye <nightshadequeen@gmail.com>
2015-08-26 15:47:14 -0500

    mostly bugfixes

--- scripts/forfun/cards.py
+++ scripts/forfun/cards.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+"""A deck of playing cards"""
+
+import sys
+from os import path
+
+if __package__ is None:
+    sys.path.append(path.dirname(path.dirname(path.realpath(__file__))))
+
 from random import shuffle
 from collections import deque
 from itertools import product
@@ -7,16 +15,17 @@ from itertools import product
 __all__ = ['Card', 'Deck']
 
 from functools import total_ordering
+from pythonutils import autoinit, autorepr
 
 @total_ordering
+@autorepr
+@autoinit
 class Card(object):
     """Virtual playing card"""
     suit_map = ['Clubs','Diamonds', 'Hearts', 'Spades']
     rank_map = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-    def __init__(self, suit: int, rank: int):
-        self.suit = suit
-        self.rank = rank
-    def __repr__(self):
+    __slots__ = ['suit', 'rank']
+    def __str__(self):
         return '<%s: %s of %s>' % (self.__class__.__name__, self.rank_map[self.rank], self.suit_map[self.suit])
     def __eq__(self, other):
         if not isinstance(other, Card): return False
@@ -35,31 +44,36 @@ class Card(object):
 class Deck(deque):
     """A deck of cards. Uses python's deque"""
     def __init__(self, cards = None, decks = 1):
+        li = [] #temporary list, to make shuffling more effective.
         if cards is None:
             for i,j,_ in product(range(4), range(13), range(decks)):
-                self.append(Card(i,j))
+                li.append(Card(i,j))
         else:
-            self.extend(cards)
-        self.shuffle()
+            li.extend(cards)
+        shuffle(li)
+        self.extend(li)
     def deal(self, pile_count : int = 2, card_count = None):
         """Deals out deck into seperate piles"""
         hands = [Deck([]) for _ in range(pile_count)]
         if card_count is None:
-            card_count = len(self)//hands + 1
+            card_count = len(self)//pile_count + 1
         try:
             for _ in range(card_count):
                 for i in hands:
-                    i.appendleft(super().popleft(self))
+                    i.appendleft(super().popleft())
         except IndexError:
             pass
         return hands
     def shuffle(self):
+        """Shuffles deck, using random.shuffle
+        (and a temporary list, for performance reasons)"""
         li = list(self)
         shuffle(li)
         self.clear()
-        for i in li:
-            self.append(i)
+        self.extend(li)
     def popleft(self, n = 1):
         return [super(Deck, self).popleft() for _ in range(n)]
     def pop(self, n = 1):
         return [super(Deck, self).pop() for _ in range(n)]
+    def __str__(self):
+        return 'deque([%s])' % ', '.join(map(str,self))
