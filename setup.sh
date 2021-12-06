52e1eb20fdc985eff2799c7ddebdb8aee9b01364
Shulin Ye <nightshadequeen@gmail.com>
2021-12-05 19:24:38 -0500

    fixes the setup script to actually install packages.

--- setup.sh
+++ setup.sh
@@ -12,19 +12,22 @@ fi
 echo "backing up current packages"
 if [ ! -d "$HOME/backups" ]; then
     mkdir -p "$HOME/backups"
-    backups="$HOME/backups"
 fi
+
+    backups="$HOME/backups"
 
 dpkg --get-selections > "$backups/$HOST-installed_packages_`date +%F`.txt"
 sudo cp /etc/apt/sources.list "$backups/$HOST-sources-`date +%F`.list"
 sudo cp /etc/apt/sources.list.d "$backups/$HOST-sources-`date +%F`.list.d" -r
 sudo apt-key exportall > "$backups/$HOST-repo-`date +%F`.keys"
 
+sudo add-apt-repository universe
+
 echo "updating repos"
 sudo apt-get update
 
 packagelist="$dotfiles/package_lists/linux_packages.txt"
-xargs -a <(awk '! /^ *(#|$)/' "$packagelist") -r -- sudo apt-get install
+xargs -L1 -a "$packagelist" -r -- sudo apt-get install
 
 if hash pip 2>/dev/null; then
     pip freeze > $backups/$HOST-pip-packages_`date +%F`.txt 2>>$error
@@ -43,6 +46,8 @@ ln -sf $dotfiles/bash_aliases $HOME/.bash_aliases
 ln -sf $dotfiles/rc_common $HOME/.rc_common
 ln -sf $dotfiles/vimrc $HOME/.vimrc
 ln -sf $dotfiles/git $HOME/.config/git
+ln -sf $dotfiles/cal.dat $HOME/.cal.dat
+ln -sf $dotfiles/cal.col $HOME/.cal.col
 
 mkdir -p $HOME/.TeXworks/templates
 ln -sf $dotfiles/Texworks/templates $HOME/.TeXworks/templates/custom
